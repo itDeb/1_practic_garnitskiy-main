17ffa244f398d3fca2355b7eb7853f4fc28599c9
itDeb <79352448+itDeb@users.noreply.github.com>
2021-05-28 20:18:20 +0300

    Initial commit

---
+++ clickbuttons/src/main/java/com/mirea/garnitskiy/clickbuttons/MainActivity.java
@@ -0,0 +1,34 @@
+package com.mirea.garnitskiy.clickbuttons;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private Button buttonOk;
+    private Button buttonCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        buttonOk = (Button) findViewById(R.id.button);
+        buttonCancel = (Button) findViewById(R.id.button2);
+
+
+        View.OnClickListener oclBtnOk = v -> tvOut.setText("Нажата кнопка ОК");
+        buttonOk.setOnClickListener(oclBtnOk);
+    }
+
+    public void onClickCancelButton(View view) {
+        tvOut.setText("Нажата кнопка Cancel");
+        Toast.makeText(this, "Eщё один способ!", Toast.LENGTH_LONG).show();
+    }
+}
